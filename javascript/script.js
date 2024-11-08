67940062e02a696a46a3a90b2a79d2b807346685
Jady le Bars <js.lebars@gmail.com>
2024-11-08 15:30:23 +0100

    booleans notes

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,7 @@ let hacking = "Happy hacking!";
 // 4. Display the variable 'hacking' as a message in the console.
 console.log(hacking);
 
--------------------------------------------------------
+// ------------------------------------------------------
 
 console.log("HelloWorld");
 
@@ -28,3 +28,67 @@ balance = balance + 1;
 balance += 1;
 balance++;
 console.log("balance after addition: ", balance);
+
+const body = document.querySelector("body");
+
+const addButton = document.querySelector("[data-js=add-button]");
+const removeButton = document.querySelector("[data-js=remove-button]");
+const toggleButton = document.querySelector("[data-js=toggle-button]");
+
+EVENTS;
+
+addButton.addEventListener("click", () => {
+  console.log("add button clicked");
+  body.classList.add("dark");
+});
+
+removeButton.addEventListener("click", () => {
+  body.classList.remove("dark");
+});
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+
+CONDITIONS & BOOLEANS;
+
+let carrots = 2;
+const dishesCleaned = 3;
+
+const kidDidHelpCleanTheDishes = dishesCleaned > 3;
+
+if (kidDidHelpCleanTheDishes) {
+  carrots = carrots + 2;
+} else {
+  carrots = carrots - 1;
+}
+
+// ternary operator
+dishesCleaned > 3 ? (carrots = carrots + 2) : (carrots = carrots - 1);
+// carrots = carrots + (dishesCleaned > 3 ? 2 : -1);
+
+// condition ? ifConditionTrue : ifConditionFalse
+
+const animal = "Dog";
+let emoji;
+
+if (animal === "Dog") {
+  emoji = "🐶";
+} else if (animal === "Tiger") {
+  emoji = "🐯";
+} else {
+  emoji = "❓";
+}
+
+// switch (animal) {
+//     case "Dog":
+//         emoji = "🐶";
+//         break;
+//     case "Tiger":
+//         emoji = "🐯";
+//         break;
+//     default:
+//         emoji = "❓";
+// }
+
+console.log("animal is: ", animal, emoji);
